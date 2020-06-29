d52b854fddde073908d7f73ad5e53bbe83d0efde
Joldiazch <54413604+Joldiazch@users.noreply.github.com>
2020-06-29 01:14:56 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import "assets/scss/material-kit-react.scss?v=1.9.0";
 // pages for this product
 import Components from "views/Components/Components.js";
 import LandingPage from "views/LandingPage/LandingPage.js";
-import ProfilePage from "views/ProfilePage/ProfilePage.js";
-import LoginPage from "views/LoginPage/LoginPage.js";
+
 
 
 // Create apollo client
